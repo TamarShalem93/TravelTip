91db7daf579b26b9d4d8fb0b726236d7ead674c4
lior88844 <lior88844@gmail.com>
2023-02-22 15:05:37 +0200

    storage

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,10 +1,26 @@
+import { utilService } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+const PET_KEY = 'petDB'
+var gFilterBy = { txt: '', minScore: 0 }
+_createPets()
+
+export const petService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyPet,
+    getFilterBy,
+    setFilterBy
+}
 export const locService = {
     getLocs
 }
 
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
 
@@ -15,5 +31,6 @@ function getLocs() {
         }, 2000)
     })
 }
+functio
 
 
